d45056ab06dc1e58e46ffe5608c35bc5113aa0eb
Pablito00 <91752643+Pablito00@users.noreply.github.com>
2023-11-27 11:38:35 +0000

    Counters.
    Tem os 2 counters.

--- main.v
+++ main.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+`timescale 1ns / 100ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -26,59 +26,78 @@ module main(CLK, RESET, START, OUT, BIST_END, RUNNING);
    
    reg enable;
    reg [7:0] count; //deve contar ate 89 = (N+1)*(M+1)-1 = (9+1)*(8+1) 
+   reg [3:0] count_N, count_M;
+   
    // state flip-flops
    reg out_reg,rst_reg,bist,run,start;
    reg [2:0] state, next_state;
+ 
    // state coding
    localparam [2:0] IDLE=0, S0=1, S1=2;
    localparam [4:0] N=8, M=9;
-     
-   always @(posedge CLK)
+    
+    always @(posedge RESET)
     begin
-        if (RESET == 1'b1)
-         state <= IDLE;
-        else
-            state <= next_state;
+         run = 0;
+         bist = 0;
+         count_N = 8'd0;
+         count_M = 8'd0;
     end
     
-    always @(posedge START or posedge bist)
-        if (START == 1)
-            start = 1;
-        else if (bist == 1)
-            start = 0;    
-    // next state calculations
+    always @(posedge CLK)
+       begin
+       if (RESET == 1'b1)
+            state <= IDLE;
+       else
+            state <= next_state;
+       end
+    
+    always @(posedge CLK)
+       begin       
+           if(run == 1'b1)
+           begin 
+                out_reg <= 1'b1;
+                count_N <= count_N + 8'd1;     
+           end
+           
+           if(count_N==N)
+           begin
+                    count_M <= count_M + 8'd1;
+                    count_N <= 0;
+                    out_reg <= 0;
+           end
+           
+           if(count_M==M)
+           begin
+                    count_M <= 0;
+                    count_N <= 0;
+                    out_reg <= 0;
+                    run <= 0;
+           end
+       end
+
     always @(*)
     begin
         case (state)
         IDLE:begin
-                if (start == 1)
+                if (START == 1)
                     next_state = S0;
-                else next_state = IDLE;
-                   enable <= 0; 
-                   rst_reg <= 0;
+               else
+               begin 
+                   next_state = IDLE;
                    run <= 0;
                    bist <= 1;
+               end
             end
-        S0: if (count == ((N+1)*(M+1)-1))   
-            begin 
-                next_state <= S1;
-                enable <= 0; 
-                rst_reg <= 0;
-                run <= 0;
-                bist <= 1;
-            end
-            else 
-            begin
-                 next_state = S0;
-                 enable <= 1'b1;
-                 run <= 1;
-                 bist <= 0;
-            end 
+        S0:begin 
+            run <= 1'b1;
+           // next_state = S1;
+           end
         S1: begin
-                next_state = S1;
                 enable <= 0;
                 run <= 0;
                 bist <= 1;   
+                next_state = IDLE;
             end 
         default:   
          begin
@@ -91,27 +110,7 @@ module main(CLK, RESET, START, OUT, BIST_END, RUNNING);
             
         endcase
     end
-   always @(posedge CLK or posedge RESET)
-   begin
-        if (RESET == 1'b1)      //reset do input
-            count <= 8'd0;
-        else if (rst_reg == 1)  //reset da maquina de estado
-             count <= 8'd0;
-        else if (enable == 1'b1)
-            count <= count + 8'd1;
-   end
-   
-   always @(*)
-   begin
-        if (enable == 1'b1)
-            if (count[4:0] == (N)) // A cada 9 operacoes de count abaixa o out 
-                out_reg <= 1'b0;
-            else 
-                out_reg <= 1'b1;
-       else
-           out_reg <= 1'b0;      
-   end
-   
+    
   assign OUT = out_reg;
   assign BIST_END = bist;
   assign RUNNING = run;

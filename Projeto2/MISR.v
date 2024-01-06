a05a38e557720fdf39100fcd6601728b1b8613b8
Victor S Macedo <victor_macedo@usp.br>
2024-01-06 17:17:37 +0000

    Adicionado bits no MISR

--- Projeto2/MISR.v
+++ Projeto2/MISR.v
@@ -1,14 +1,20 @@
 `timescale 1ns / 1ps
 
-module MISR(CLK,e0,e1,e2,h0,h1,h2,hf);
+module MISR(CLK,e0,e1,e2,hf);
 input CLK,e0,e1,e2;
-output reg h0,h1,h2,hf;
+reg h0,h1,h2,h3,h4,h5,h6,h7;
+output reg [2:0] hf;
 
 always @(posedge CLK)
 begin
     h0 <= !(e2 || h1);
     h1 <= !(e1 || h0);
     h2 <= !(e0 || !(h2 || !(h1||h0))); 
-    hf <= h2 & h1 & h;
+    h3 <= !(h2 || h1);
+    h4 <= !(h3 || h2);
+    h5 <= !(h4 || h3);
+    h6 <= !(h5 || h4);
+    h7 <= !(h6 || h5);
+    hf <= {h7,h6,h5,h4,h3,h2, h1, h0};
 end
 endmodule

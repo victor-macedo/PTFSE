07c51365a3840222047d22639039d14465694c54
Victor S Macedo <victor_macedo@usp.br>
2024-01-07 01:03:08 +0000

    de volta ao 5h

--- Projeto2/Comparador.v
+++ Projeto2/Comparador.v
@@ -2,14 +2,14 @@
 
 module Comparador(CLK, RST,Finish, hf,passnfail);
 input CLK,RST,Finish;
-input [14:0]hf;
+input [4:0]hf;
 output reg passnfail;
 
 always @(*)
 begin
     if (Finish == 1)
     begin
-        if (hf == 15'b10010011100000) //Valor obtido atraves da simulacao
+        if (hf == 4'b10010011100000) //Valor obtido atraves da simulacao
         passnfail <= 1;
         else
         passnfail <= 0;

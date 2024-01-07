613a20240ace2675689fb681447a8c344402df2b
Victor S Macedo <victor_macedo@usp.br>
2024-01-07 14:08:35 +0000

    Alterado porta Xor

--- Projeto2/main.v
+++ Projeto2/main.v
@@ -15,7 +15,7 @@ circuito12 circuito_scan(.clk(CLK),.rst(RST),.k(circ_k),.j(circ_j),.rx_en(circ_e
      .scan_in(in_x0), .scan_out(scan_out));
 LFSR LFSR_in (.CLK(CLK), .RST(RST),.Seed(Seed),.x0(in_x0),.x1(in_x1),.x2(in_x2)); //LFSR na entrada do circuito
 Bist_control Bist(.CLK(CLK), .RESET(RST), .START(bist_start), .OUT(bist_out), .BIST_END(bist_end), .RUNNING(),.Seed(Seed),.FINISH(Finish));//INit deve reiniciar o scan,Finish � util para saber quando fazer pass_fail
-MISR MISR(.CLK(CLK),.e0(scan_out),.e1(out_synced_d),.e2(out_sync_err_d),.hf(hf));
+MISR MISR(.CLK(CLK),.RST(RST), .e0(scan_out),.e1(out_synced_d),.e2(out_sync_err_d),.hf(hf));
 Comparador Comp(.CLK(CLK),.RST(RST),.Finish(Finish), .hf(hf),.passnfail(pass_fail),.bist_end(bist_end));
 always @(*) //Mux da entrada dos dados
 begin    

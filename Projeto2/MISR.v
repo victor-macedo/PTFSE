447a05cb2dfcbde6888afde313e019378ae75fa2
Victor S Macedo <victor_macedo@usp.br>
2024-01-07 09:51:07 +0000

    Mudança no polinomio e geração do MISR

--- Projeto2/MISR.v
+++ Projeto2/MISR.v
@@ -2,14 +2,22 @@
 
 module MISR(CLK,e0,e1,e2,hf);
 input CLK,e0,e1,e2;
-reg h0,h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h20;
-output reg [20:0] hf;
+reg h0,h1,h2,h3,h4,h5,h6,h7,h8  ;
+output reg [6:0] hf;
 
 always @(posedge CLK)
 begin
-    h0 <= !(e2 || h1);
-    h1 <= !(e1 || h0); 
-    h2 <= !(e0 || !(h2 || !(h1||h0))); 
+    h0 <= h1;
+    h1 <= h2;
+    h2 <= h3;
+    h3 <= h4;
+    h4 <= !(e2 || h5);
+    h5 <= !(e1 || h6);
+    h6 <= !(e0 || !(h5 || !(h4 || !(h3 || !(h2 || !(h1 ||h0 )))))); 
+
+//    h0 <= !(e2 || h1);
+//    h1 <= !(e1 || h0); 
+//    h2 <= !(e0 || !(h2 || !(h1||h0))); 
 //    h3 <= !(h2 || h1);
 //    h4 <= !(h3 || h2);
 //    h5 <= !(h4 || h3);
@@ -28,6 +36,6 @@ begin
 //    h18 <= !(h17 || h14);
 //    h19 <= !(h18 || h15);
 //    h20 <= !(h19 || h16);
-    hf <= {h0,h1,h2};
+    hf <= {h0,h1,h2,h3,h4,h5,h6};
 end
 endmodule

34f6f48f2b54765d522bc1db2a753eae47d41275
Pablito00 <91752643+Pablito00@users.noreply.github.com>
2024-01-07 14:07:58 +0000

    nova test_bench

--- Projeto2/para_guardar/test_bench.v
+++ Projeto2/para_guardar/test_bench.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 100ps
 
-module teste_main;
+module main_test_bench;
 
 reg clk, rst, bist_start, k, j, en;
-wire pass_fail, BIST_END,synced_d,err_d;
+wire pass_nfail, bist_end,synced_d,err_d;
 
 main under_test(.CLK(clk),.RST(rst), .bist_start(bist_start),
                 .in_k(k),.in_j(j),.in_en(en), .out_synced_d(synced_d),.out_sync_err_d(err_d),
-                .pass_fail(pass_fail), .bist_end(BIST_END));
+                .pass_fail(pass_nfail), .bist_end(bist_end));
 
 initial
 begin  
@@ -21,14 +21,14 @@ begin
         
 end
 
-always #50 clk=~clk;
+always #60 clk=~clk;
 
 initial
 begin
     #0 bist_start = 0;       //0
     #300 bist_start = 1;     //300
     #300 bist_start = 0;
-    wait(BIST_END) 
+    wait(bist_end) 
     #600
     $finish;      //10300
 end
